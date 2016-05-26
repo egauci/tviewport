30f1e7acd4695e62b2a7673ab0d3f2459cc2994c
Ed Gauci <ed@gauci.co>
2016-05-26 12:20:51 -0700

    implemented

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var viewport = require('../dist/index.js');
+var viewport = require('viewport-event').default;
 
 var ctr = document.getElementById('overlay');
+var topleft = document.getElementById('topleft');
+var topright = document.getElementById('topright');
+var bottomleft = document.getElementById('bottomleft');
+var bottomright = document.getElementById('bottomright');
+
+var offsetHeight, offsetWidth;
 
 function show(vp) {
+  offsetHeight = offsetHeight ? offsetHeight : ctr.offsetHeight;
+  offsetWidth = offsetWidth ? offsetWidth : ctr.offsetWidth;
+  var top = String(Math.floor((vp.clientHeight - offsetHeight) / 2) + vp.scrollY) + 'px';
+  var left = String(Math.floor((vp.clientWidth - offsetWidth) / 2) + vp.scrollX) + 'px';
+  ctr.style.top = top;
+  ctr.style.left = left;
+  // console.log(top, left, offsetHeight, offsetWidth);
   var html = '<pre>\n' +
     'scrollY:      ' + vp.scrollY + '\n' +
     'scrollX:      ' + vp.scrollX + '\n' +
@@ -10,8 +23,17 @@ function show(vp) {
     'height:       ' + vp.height + '\n' +
     'clientWidth:  ' + vp.clientWidth + '\n' +
     'clientHeight: ' + vp.clientHeight + '\n' +
+    'top, left:    ' + top + ', ' + left + '\n' +
     '</pre>\n';
   ctr.innerHTML = html;
+  topleft.style.top = String(vp.scrollY + 2) + 'px';
+  topleft.style.left = String(vp.scrollX + 2) + 'px';
+  topright.style.top = String(vp.scrollY + 2) + 'px';
+  topright.style.left = String(vp.scrollX + vp.clientWidth - 12) + 'px';
+  bottomleft.style.top = String(vp.scrollY + vp.clientHeight - 12) + 'px';
+  bottomleft.style.left = String(vp.scrollX + 2) + 'px';
+  bottomright.style.top = String(vp.scrollY + vp.clientHeight - 12) + 'px';
+  bottomright.style.left = String(vp.scrollX + vp.clientWidth - 12) + 'px';
 }
 
 show(viewport.getViewport());
